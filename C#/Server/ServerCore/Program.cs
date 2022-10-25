3276a79a2429f47c2ead4a443aaadbde75459437
Egyeom <ug4306@gmail.com>
2022-10-25 14:40:48 +0900

    C# Thread, ThreadPool, Task

---
+++ C#/Server/ServerCore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            int i = 5;
+            while(--i > 0)
+                Console.WriteLine("Hello Thread!");
+        }
+        static void Main(string[] args)
+        {
+            Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning); // TaskCreationOptions.LongRunning옵션을 주게되면 ThreadPool과는 별도로 처리함
+            //Task도 ThreadPool에서 같이 동작되는 상황
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for(int i =0; i < 4; i++)
+            {
+                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });    
+            }
+            ThreadPool.QueueUserWorkItem(MainThread); //.NET Framework에서 지원하는 thread pool
+
+            
+            //Thread t = new Thread(MainThread);
+            //t.Name = "Test Thread"; // 쓰레드 이름 지정
+            //t.IsBackground = true; // 메인쓰레드 종료화 함께 종료 되느냐 안되느냐
+            //t.Start();
+
+            //t.Join();
+
+            //Console.WriteLine("Hello World!");
+            while(true)
+            {
+
+            }
+        }
+    }
+}
---
+++ C++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,101 @@
+﻿//100 0 # 100
+//6 16 # 3
+//8 20 # 3
+//15964 89498 # 4781
+//3 43 # 6
+//4 27 # 5
+//5 35 # 5
+//6 43 # 5
+//7 43 # 6
+//100 1 # 99
+//10 19 # 2
+//5 19 # 3
+//1 20 # 5
+//100000 100000 # 0
+//0 100000 # 22
+//100000 0 # 100000
+//0 1 # 1
+//3482 45592 # 637
+//2 4 # 1
+//9 5 # 4
+//5 5 # 0
+//5 17 # 4
+
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#define MAX 100001
+vector<int> v;
+
+void bfs(int start, int target)
+{
+    queue<pair<int, int>> q;
+    q.push({ start,0 });
+
+    while (!q.empty())
+    {
+        int first = q.front().first;
+        int second = q.front().second;
+        if (first == target)
+        {
+            cout << second << "\n";
+            break;
+        }
+        q.pop();
+        
+        //if (v[first] == 0) v[first] = second;
+        //else if (first > target)
+        //{
+        //    int tmp = first - target + second;
+        //    v[first] = min(tmp, v[first]);
+        //    continue;
+        //}
+        //else if (v[first] > second) v[first] = second;
+        //else continue;
+
+        if (first - 1 >= 0 && v[first - 1] == 0)
+        {
+            v[first - 1] = second + 1;
+            q.push({ first - 1,second + 1 });
+        }
+        if(first + 1 < MAX && v[first + 1] == 0)
+        {
+            v[first + 1] = second + 1;
+            q.push({ first + 1,second + 1 });
+        }
+        if(first * 2 > 0 && first * 2 < MAX && v[first * 2] == 0)
+        {
+            int tmp = 0;
+            if (first * 2 > target)
+            {
+                tmp = first * 2 - target + second;
+            }
+            else tmp = second + 1;
+            v[first * 2] = tmp;
+            q.push({ first * 2,second + 1 });
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, k;
+
+    cin >> n >> k;
+    if (n >= k)
+    {
+        cout << n - k << "\n";
+        return 0;
+    }
+    v = move(vector<int>(MAX, 0));
+    bfs(n, k);
+
+    return 0;
+}
